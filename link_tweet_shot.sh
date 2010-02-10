86357a8bd984eda9fc26e8b84fcedb6d79905d1c
Chinese Shot <chineseshot@gmail.com>
2010-02-10 21:15:43 +0000

    Work mode.

--- link_tweet_shot.sh
+++ link_tweet_shot.sh
@@ -6,14 +6,14 @@ PYTHON26=/usr/bin/python2.6
 USERNAME=pageshot
 PASSWORD=password
 
-LTS_DIR=`basename $0`
+LTS_DIR=`dirname $0`
 export PATH=$PATH:${LTS_DIR}
 
-nohup failover.sh ${PYTHON25} ${LTS_DIR}/shot_service.py -n 20 -t 30 \
+nohup ${LTS_DIR}/failover.sh ${PYTHON25} ${LTS_DIR}/shot_service.py -n 20 -t 30 \
     -l ${LTS_DIR}/link_shot_tweet_log.conf &
-nohup failover.sh ${PYTHON25} ${LTS_DIR}/rt_shot.py -u ${USERNAME} -p ${PASSWORD} \
-    -l ${LTS_DIR}/link_shot_tweet_log.conf -d -k &
-nohup failover.sh task_gc.py &
-nohup failover.sh ${PYTHON25} ${LTS_DIR}/url_processor.py \
+nohup ${LTS_DIR}/failover.sh ${PYTHON25} ${LTS_DIR}/rt_shot.py -u ${USERNAME} -p ${PASSWORD} \
+    -l ${LTS_DIR}/link_shot_tweet_log.conf &
+nohup ${LTS_DIR}/failover.sh task_gc.py &
+nohup ${LTS_DIR}/failover.sh ${PYTHON25} ${LTS_DIR}/url_processor.py \
     -p ${LTS_DIR}/link_tweet_shot.sqlite \
     -l ${LTS_DIR}/link_shot_tweet_log.conf &
