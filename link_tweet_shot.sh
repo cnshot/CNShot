71a616836baa8e50a2a2270d9ef90d8bb81b47be
Chinese Shot <chineseshot@gmail.com>
2010-02-11 11:02:53 +0000

    Reduced shot service workers number.

--- link_tweet_shot.sh
+++ link_tweet_shot.sh
@@ -9,7 +9,7 @@ PASSWORD=password
 LTS_DIR=`dirname $0`
 export PATH=$PATH:${LTS_DIR}
 
-nohup ${LTS_DIR}/failover.sh ${PYTHON25} ${LTS_DIR}/shot_service.py -n 20 -t 30 \
+nohup ${LTS_DIR}/failover.sh ${PYTHON25} ${LTS_DIR}/shot_service.py -n 8 -t 30 \
     -l ${LTS_DIR}/link_shot_tweet_log.conf &
 nohup ${LTS_DIR}/failover.sh ${PYTHON25} ${LTS_DIR}/rt_shot.py -u ${USERNAME} -p ${PASSWORD} \
     -l ${LTS_DIR}/link_shot_tweet_log.conf &
