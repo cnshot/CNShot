9ad4926da4b77eb9cdc75daf5ec68fdb08900846
Chinese Shot <chineseshot@gmail.com>
2010-02-25 04:57:17 +0000

    Refined fetch_tweet_link code

--- fetch_tweet_link.py
+++ fetch_tweet_link.py
@@ -10,6 +10,7 @@ import sys
 import pickle
 import memcache
 import time, rfc822
+import logging, logging.config
 
 from optparse import OptionParser
 from stompy.simple import Client
@@ -18,6 +19,81 @@ from chinese_detecting import isChinesePhase
 
 #os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 from lts.models import Link, Tweet
+
+def fetchTweetLink():
+    url_pattern = re.compile('((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)')
+    stomp = Client()
+    stomp.connect()
+    mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+
+    if options.since_file and ( not options.since ):
+        try:
+            with open(options.since_file, 'r') as f:
+                try:
+                    options.since=int(f.read())
+                except:
+                    options.since=None
+        except:
+            pass
+
+    api = twitter.Api(username=options.username, password=options.password)
+    if options.since:
+        logger.info("Since: %d", options.since)
+        statuses = api.GetFriendsTimeline(options.username, count=options.count, since_id=options.since)
+    else:
+        statuses = api.GetFriendsTimeline(options.username, count=options.count)
+    for s in statuses:
+        if s.user.screen_name == options.username :
+            # don't RT myself
+            continue
+        if not isChinesePhase(s.text.encode("utf-8", "ignore")):
+            # Chinese tweet only
+            # print "Skip none Chinese tweet: %s" % s.text
+            continue
+        matches = re.findall(url_pattern,s.text)
+        if matches:
+            logger.debug("Tweet with link(s): %d %s %s %s",
+                         s.id, s.user.screen_name,
+                         str(s.created_at), s.text.encode('utf-8'))
+
+            ls = []
+
+            for m in matches:
+                task_id = str(uuid.uuid1())
+                mc.set(task_id, s, time=600)
+
+                # update Tweet and Link
+                try:
+                    l = Link.objects.get(url__exact=m[0])
+                except Link.DoesNotExist:
+                    l = Link(url=m[0])
+                    l.save()
+
+                # NOTICE: l record may be updated by url_processor after stomp.put
+                ls.append(l)
+                
+                stomp.put(pickle.dumps({'id':task_id,
+                                        'url':m[0],
+                                        'filename':None}),
+                          destination=options.dest_queue)
+
+            # update Tweet
+            t = Tweet(id = s.id,
+                      text = s.text,
+                      created_at = datetime.fromtimestamp(time.mktime(rfc822.parsedate(s.created_at))),
+                      user_screenname = s.user.screen_name)
+            t.save()
+            t.links = map(lambda x: x.id, ls)
+            t.save()
+                
+    if statuses:
+        logger.info("Latest status ID: %d", statuses[0].id)
+        if options.since_file:
+            try:
+                with open(options.since_file, 'w') as f:
+                    f.write(str(statuses[0].id))
+            except:
+                pass    
 
 if __name__ == '__main__':
     description = '''Fetch Twitter timeline and enqueue links.'''
@@ -54,80 +130,22 @@ if __name__ == '__main__':
                       help="Twitter password [default: %default].",
                       metavar="PASSWORD")
 
-
+    parser.add_option("-l", "--log-config",
+                      dest="log_config", 
+                      default="/etc/link_shot_tweet_log.conf",
+                      type="string",
+                      help="Logging config file [default: %default].",
+                      metavar="LOG_CONFIG")
 
     (options,args) = parser.parse_args()
     if len(args) != 0:
         parser.error("incorrect number of arguments") 
 
-    url_pattern = re.compile('((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)')
-    stomp = Client()
-    stomp.connect()
-    mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+    # walk around encoding issue
+    reload(sys)
+    sys.setdefaultencoding('utf-8') 
 
-    if options.since_file and ( not options.since ):
-        try:
-            with open(options.since_file, 'r') as f:
-                try:
-                    options.since=int(f.read())
-                except:
-                    options.since=None
-        except:
-            pass
+    logging.config.fileConfig(options.log_config)
+    logger = logging.getLogger("fetch_tweet_link")
 
-    api = twitter.Api(username=options.username, password=options.password)
-    if options.since:
-        print "Since: ", options.since
-        statuses = api.GetFriendsTimeline(options.username, count=options.count, since_id=options.since)
-    else:
-        statuses = api.GetFriendsTimeline(options.username, count=options.count)
-    for s in statuses:
-        if s.user.screen_name == options.username :
-            # don't RT myself
-            continue
-        if not isChinesePhase(s.text.encode("utf-8", "ignore")):
-            # Chinese tweet only
-            # print "Skip none Chinese tweet: %s" % s.text
-            continue
-        matches = re.findall(url_pattern,s.text)
-        if matches:
-            print s.id, " ", s.user.screen_name, " ", s.created_at, " ", s.text.encode('utf-8')
-
-            ls = []
-
-            for m in matches:
-                task_id = str(uuid.uuid1())
-                mc.set(task_id, s, time=600)
-
-                # update Tweet and Link
-                try:
-                    l = Link.objects.get(url__exact=m[0])
-                except Link.DoesNotExist:
-                    l = Link(url=m[0])
-                    l.save()
-
-                # NOTICE: l record may be updated by url_processor after stomp.put
-                ls.append(l)
-                
-                stomp.put(pickle.dumps({'id':task_id,
-                                        'url':m[0],
-                                        'filename':None}),
-                          destination=options.dest_queue)
-
-            # update Tweet
-            t = Tweet(id = s.id,
-                      text = s.text,
-                      created_at = datetime.fromtimestamp(time.mktime(rfc822.parsedate(s.created_at))),
-                      user_screenname = s.user.screen_name)
-            t.save()
-            t.links = map(lambda x: x.id, ls)
-            t.save()
-                
-    if statuses:
-        print statuses[0].id
-        if options.since_file:
-            try:
-                with open(options.since_file, 'w') as f:
-                    f.write(str(statuses[0].id))
-            except:
-                pass
+    fetchTweetLink()
