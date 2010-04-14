6b9972cb2b84885909f87cdab734f7fb60745152
Chinese Shot <chineseshot@gmail.com>
2010-04-14 12:19:12 +0000

    Replaced most options with config file.

--- link_tweet_shot.sh
+++ link_tweet_shot.sh
@@ -3,18 +3,12 @@
 PYTHON25=/usr/bin/python2.5
 PYTHON26=/usr/bin/python2.6
 
-USERNAME=cnshot
-PASSWORD=password
-
 LTS_DIR=`dirname $0`
 export PATH=$PATH:${LTS_DIR}
 export DJANGO_SETTINGS_MODULE=settings
 export PYTHONPATH=${LTS_DIR}/lts_web
 
-nohup ${LTS_DIR}/failover.sh ${LTS_DIR}/shot_service.py -n 4 -t 30 \
-    -l ${LTS_DIR}/link_shot_tweet_log.conf &
-nohup ${LTS_DIR}/failover.sh ${LTS_DIR}/rt_shot.py -u ${USERNAME} -p ${PASSWORD} \
-    -l ${LTS_DIR}/link_shot_tweet_log.conf &
+nohup ${LTS_DIR}/failover.sh ${LTS_DIR}/shot_service.py &
+nohup ${LTS_DIR}/failover.sh ${LTS_DIR}/rt_shot.py &
 nohup ${LTS_DIR}/failover.sh task_gc.py &
-nohup ${LTS_DIR}/failover.sh ${LTS_DIR}/url_processor.py \
-    -l ${LTS_DIR}/link_shot_tweet_log.conf &
+nohup ${LTS_DIR}/failover.sh ${LTS_DIR}/url_processor.py &
