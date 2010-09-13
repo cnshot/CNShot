b2aae9dfd7882a77e3e04a4ebffd71d13dd89b79
Chinese Shot <chineseshot@gmail.com>
2010-09-13 06:43:17 +0000

    replace len() with count()

--- data_archive.py
+++ data_archive.py
@@ -12,15 +12,15 @@ class DataArchiver:
     def deleteExpiredShotCaches(cls, threshold_timestamp):
         # delete shot caches without valid LinkShot
         scs = ShotCache.objects.filter(linkshot=None)
-        if len(scs) > 0:
+        if scs.count() > 0:
             logger.debug('deleteExpiredShotCaches: deleting %d ShotCaches without valid LinkShot...',
-                         len(scs))
+                         scs.count())
         scs.delete()
 
         # delete shot caches with LinkRate older than threshold_timestamp
         lrs = LinkRate.objects.filter(rating_time__lt=threshold_timestamp)
-        if len(lrs) > 0:
-            logger.debug('deleteExpiredShotCaches: %d expired LinkRates', len(lrs))
+        if lrs.count() > 0:
+            logger.debug('deleteExpiredShotCaches: %d expired LinkRates', lrs.count())
         for lr in lrs:
             l = lr.link.getRoot()
             try:
@@ -30,37 +30,37 @@ class DataArchiver:
             if ls.url is not None:
                 continue
             scs = ShotCache.objects.filter(linkshot=ls)
-            logger.debug('deleteExpiredShotCaches: deleting %d ShotCaches with linkshot %d...', len(scs), ls.id)
+            logger.debug('deleteExpiredShotCaches: deleting %d ShotCaches with linkshot %d...', scs.count(), ls.id)
             scs.delete()
 
         # delete shot caches with LinkShot older than threshold_timestamp
         lss = LinkShot.objects.filter(shot_time__lt=threshold_timestamp)
-        logger.debug('deleteExpiredShotCaches: %d expired LinkShots', len(lss))
+        logger.debug('deleteExpiredShotCaches: %d expired LinkShots', lss.count())
         for ls in lss:
             scs = ShotCache.objects.filter(linkshot=ls)
-            logger.debug('deleteExpiredShotCaches: deleting %d ShotCaches with linkshot %d...', len(scs), ls.id)
+            logger.debug('deleteExpiredShotCaches: deleting %d ShotCaches with linkshot %d...', scs.count(), ls.id)
             scs.delete()
 
     @classmethod
     def deleteExpiredLinkRates(cls, threshold_timestamp):
         # delete linkrate without link
         lrs = LinkRate.objects.filter(link=None)
-        if len(lrs) > 0:
-            logger.debug('deleteExpiredLinkRates: deleting %d LinkRates without link...', len(lrs))
+        if lrs.count() > 0:
+            logger.debug('deleteExpiredLinkRates: deleting %d LinkRates without link...', lrs.count())
         lrs.delete()
 
         # delete linkrate with expired rating_time
         lrs = LinkRate.objects.filter(rating_time__lt=threshold_timestamp)
-        if len(lrs) > 0:
-            logger.debug('deleteExpiredLinkRates: deleting %d expired LinkRates...', len(lrs))
+        if lrs.count() > 0:
+            logger.debug('deleteExpiredLinkRates: deleting %d expired LinkRates...', lrs.count())
         lrs.delete()
 
     @classmethod
     def deleteExpiredLinkShots(cls, threshold_timestamp):
         # delete linkshot without link
         lss = LinkShot.objects.filter(link=None)
-        if len(lss) > 0:
-            logger.debug('deleteExpiredLinkShots: deleting %d LinkShots without link...', len(lss))
+        if lss.count() > 0:
+            logger.debug('deleteExpiredLinkShots: deleting %d LinkShots without link...', lss.count())
         for ls in lss:
             ls.shotcache_set.clear()
             ls.shotpublish_set.clear()
@@ -70,8 +70,8 @@ class DataArchiver:
 
         # delete linkshot with expired shot_time
         lss = LinkShot.objects.filter(shot_time__lt=threshold_timestamp)
-        if len(lss) > 0:
-            logger.debug('deleteExpiredLinkShots: deleting %d expired LinkShots', len(lss))
+        if lss.count() > 0:
+            logger.debug('deleteExpiredLinkShots: deleting %d expired LinkShots', lss.count())
         for ls in lss:
             logger.debug('Deleting sets of LinkShot: %d %s %s',
                          ls.id, ls.link, ls.url)
@@ -85,8 +85,8 @@ class DataArchiver:
     def deleteExpiredTweets(cls, threshold_timestamp):
         # delete expired tweets
         ts = Tweet.objects.filter(created_at=threshold_timestamp)
-        if len(ts) > 0:
-            logger.debug('deleteExpiredTweets: deleting %d expired Tweets', len(ts))
+        if ts.count() > 0:
+            logger.debug('deleteExpiredTweets: deleting %d expired Tweets', ts.count())
         for t in ts:
             t.linkshot_set.clear()
 
